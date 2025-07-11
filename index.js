c2296872fe4d6c2331d1b6575b84cda50f33bca8
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-07-11 23:43:58 +0600

    Project initialized

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 3000;
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Aroggo server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Aroggo server is running on port,${port}`);
+});
